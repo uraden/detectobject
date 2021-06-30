cf62c8f7837cb30a95c4c04b93b0950ec8c5e8f7
Jamal Fakhriddinov <jamalfakhriddinov@Jamals-MacBook-Pro.local>
2021-06-30 11:58:33 +0800

    Updates

---
+++ precache-manifest.f376c169bce6a214e9d0de68480c27e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d25e14d1240a0c6655bacb55f423c84",
+    "url": "/detectobject/index.html"
+  },
+  {
+    "revision": "d96ab507adb8a5820632",
+    "url": "/detectobject/static/css/main.4f8667db.chunk.css"
+  },
+  {
+    "revision": "420180d214cc13b5ec4e",
+    "url": "/detectobject/static/js/2.24d5fba1.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/detectobject/static/js/2.24d5fba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96ab507adb8a5820632",
+    "url": "/detectobject/static/js/main.906017c3.chunk.js"
+  },
+  {
+    "revision": "b1c9f57892d65254433d",
+    "url": "/detectobject/static/js/runtime-main.9006005c.js"
+  }
+]);
